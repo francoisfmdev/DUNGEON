ac287b3028e932148b19d4e4e8f6fb9b9521058c
Francois MAESTRATI <francois.maestrati@gmail.com>
2025-04-18 14:31:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-MAINimport pygame
+import pygame
 
 TILE_SIZE = 16
 MAP_WIDTH = 20
@@ -6,35 +6,39 @@ MAP_HEIGHT = 20
 SCREEN_WIDTH = TILE_SIZE * MAP_WIDTH
 SCREEN_HEIGHT = TILE_SIZE * MAP_HEIGHT
 
-map = [
+# --- Map 20x20 remplie de 0 (sols) ---
+map_data = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
 ]
+
 # --- Pygame init ---
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Map 20x20 custom")
-floor_img = pygame.image.load("WHITE.png")
-wall_img = pygame.image.load("GREY.png")
+
+floor_img = pygame.image.load("WHITE.png").convert()
+wall_img = pygame.image.load("GREY.png").convert()
+floor_img = pygame.transform.scale(floor_img, (TILE_SIZE, TILE_SIZE))
+wall_img = pygame.transform.scale(wall_img, (TILE_SIZE, TILE_SIZE))
 
 # --- Boucle principale ---
 clock = pygame.time.Clock()
@@ -45,17 +49,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # Affichage de la carte
-        for y in range(MAP_HEIGHT):
-            for x in range(MAP_WIDTH):
-                pos = (x * TILE_SIZE, y * TILE_SIZE)
-                tile = map[y][x]
-                if tile == 1:
-                    screen.blit(wall_img, pos)
-                else:
-                    screen.blit(floor_img, pos)
+    # Affichage de la carte
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            pos = (x * TILE_SIZE, y * TILE_SIZE)
+            tile = map_data[y][x]
+            if tile == 1:
+                screen.blit(wall_img, pos)
+            else:
+                screen.blit(floor_img, pos)
 
-        pygame.display.flip()
-        clock.tick(60)
+    pygame.display.flip()
+    clock.tick(60)
 
-    pygame.quit()
+pygame.quit()
