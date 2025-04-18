ba1e68f5fc61bba3ababdc8a47cfa4100d6d3e23
Francois MAESTRATI <francois.maestrati@gmail.com>
2025-04-18 14:28:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,61 @@
+MAINimport pygame
+
+TILE_SIZE = 16
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
+SCREEN_WIDTH = TILE_SIZE * MAP_WIDTH
+SCREEN_HEIGHT = TILE_SIZE * MAP_HEIGHT
+
+map = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+]
+# --- Pygame init ---
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Map 20x20 custom")
+floor_img = pygame.image.load("WHITE.png")
+wall_img = pygame.image.load("GREY.png")
+
+# --- Boucle principale ---
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Affichage de la carte
+        for y in range(MAP_HEIGHT):
+            for x in range(MAP_WIDTH):
+                pos = (x * TILE_SIZE, y * TILE_SIZE)
+                tile = map[y][x]
+                if tile == 1:
+                    screen.blit(wall_img, pos)
+                else:
+                    screen.blit(floor_img, pos)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
